dc0cd48e8b8541c3c6a524634bab008e19a483cf
Boyd Querubin <boyd@ezlynkna.com>
2024-02-01 14:20:30 -0700

    first commit

---
+++ hasUniqueChars.js
@@ -0,0 +1,14 @@
+// Write your code below
+function hasUniqueChars(word) {
+    const uniqueChars = new Set();
+  
+    for (let i = 0; i < word.length; i++) {
+      const char = word.charAt(i);
+      if (uniqueChars.has(char)) {
+        return false; // Found a duplicate character
+      }
+      uniqueChars.add(char);
+    }
+  
+    return true; // No duplicate characters found
+  }
